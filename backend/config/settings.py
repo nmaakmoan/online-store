e2eb88ba1e8b5de01c83cac34fee1c94ab8e6d67
username <mail@ex.com>
2026-09-24 18:07:11 +0500

    feat: add pagination to rest api

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -86,6 +86,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.AllowAny",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 9,
 }
 
 SIMPLE_JWT = {
